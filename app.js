405f774ca8c7bcd09ae2534a64dd6055c91995b7
Haithem Mihoubi <64318421+haithemmihoubi@users.noreply.github.com>
2022-08-12 09:38:26 +0100

    mongo bd container

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ let path = require('path');
 let mongoose = require('mongoose');
 // connect to the database
 const  DB_USER = 'root';
-const  DB_PASSWORD = 'example';
+const  DB_PASSWORD = 'root';
 const  DB_HOST = 'mongo';
 const  DB_PORT = '27017';
-const  DB_NAME = 'test';
-const  DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
-mongoose.connect((DB_URL).then(() => console.log('Connected to the database')).catch(err => console.log(err)));
+var  DB_URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose.connect(DB_URL);
 let PORT = process.env.PORT || 3000;
 let app= express();
 app.get('/', function(req, res){
